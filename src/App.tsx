7ff10697c26e8a70b413beb8d6a81e48dc7de81b
ArtofNuel <artofnuel@gmail.com>
2023-01-05 13:45:38 +0100

    Login Page Complete.
    Mobile responsive at 400px.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './pages/Dashboard';
 import LogIn from './pages/LogIn';
 
 function App() {
   return (
     <div className="App">
       <LogIn />
+      <Dashboard />
     </div>
   );
 }
